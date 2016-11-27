c838e6e43d46d746e8f0ce47dc92c3527506c87c
Bartłomiej Hirsz <bartek.hirsz@gmail.com>
2016-11-27 20:26:04 +0100

    Map display
    Added map controller and view. It displays 3 gps points around Galeria
    Bałtycka and list of points from gps database.

--- SOZA-web/Controllers/MapController.cs
+++ SOZA-web/Controllers/MapController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SOZA_web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace SOZA_web.Controllers
 {
@@ -17,7 +20,50 @@ namespace SOZA_web.Controllers
 
         public ActionResult LocationHistory()
         {
-            return View();
+
+            //Set loc in Map model through GPS Trace DB
+            var model = new Map
+            {
+                Loc = new List<Location>()
+            };
+
+            Location location = new Location
+            {
+                Caption = "test",
+                Latitude = 54.382842,
+                Longitude = 18.600420
+            };
+            model.Loc.Add(location);
+            Location location2 = new Location
+            {
+                Caption = "test2",
+                Latitude = 54.382797,
+                Longitude = 18.598535
+            };
+            model.Loc.Add(location2);
+            Location location3 = new Location
+            {
+                Caption = "test3",
+                Latitude = 54.383428,
+                Longitude = 18.597044
+            };
+            model.Loc.Add(location3);
+
+          //  ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            var db = ApplicationDbContext.Create();
+            var GPSTraces = db.GPSTraces;
+
+            foreach(var row in GPSTraces.ToList())
+            {
+                Location locationDb = new Location
+                {
+                    Latitude = row.Latitude,
+                    Longitude = row.Longitude
+                };
+                model.Loc.Add(locationDb);
+            }
+
+            return View(model);
         }
 
         public ActionResult SafeAreasPanel()
---
+++ SOZA-web/Models/ViewModels/MapModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOZA_web.Models
+{
+    public class Map
+    {
+        public List<Location> Loc { get; set; }//IList?
+
+    }
+    public class Location
+    {
+        public string Caption { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
